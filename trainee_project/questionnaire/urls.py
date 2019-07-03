4623c268a425027ed48d4b33e7d40906c3596568
salahdin zeberga <salahdinzeberga@admins-MacBook-Air-7.local>
2019-07-03 16:12:19 +0200

    added form

--- trainee_project/questionnaire/urls.py
+++ trainee_project/questionnaire/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('eligable/', views.eligability, name="eligability"),
+    path('', views.eligability, name="eligability"),
 ]
--- trainee_project/questionnaire/views.py
+++ trainee_project/questionnaire/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms.form import SubjectForm
+from .models import Subject
 # Create your views here.
 
 
@@ -8,7 +9,13 @@ def eligability(request):
         form = SubjectForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
+            """
+            age = post['age'].value()
+            if Subject.is_minor(age):
+                return render(request,'participant.html', {'age': age})
+            """
             post.save()
-
-        else:
-            form = SubjectForm()
+            return redirect('eligability')
+    else:
+        form = SubjectForm()
+    return render(request, 'participant.html', {'form': form})
